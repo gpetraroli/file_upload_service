e80144988d66d6da71652897ebcf1d8f74f31913
g.petraroli <g.petraroli@gmail.com>
2024-02-05 16:43:05 +0100

    fix script error

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+
+composer install
+php ./bin/console importmap:install
+
+# if needs to build sass
+php ./bin/console sass:build
+
 php ./bin/console d:m:m --no-interaction
 php ./bin/console cache:clear
 
