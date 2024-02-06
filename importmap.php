2aaaef7b93f5f57b1658bdcff83adb3ddb7f2231
g.petraroli <g.petraroli@gmail.com>
2024-02-06 21:48:48 +0100

    gitignore update

--- importmap.php
+++ importmap.php
@@ -18,11 +18,11 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+    ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
     ],
     '@hotwired/turbo' => [
         'version' => '7.3.0',
