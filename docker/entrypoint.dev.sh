1bdbdf06ad050a4ad3c14dd05bb7f99dcab81ca9
g.petraroli <g.petraroli@gmail.com>
2024-02-06 14:47:34 +0100

    wip

--- docker/entrypoint.dev.sh
+++ docker/entrypoint.dev.sh
@@ -4,7 +4,7 @@ composer install
 php ./bin/console importmap:install
 
 # if needs to build sass
-php ./bin/console sass:build
+./bin/console sass:build
 
 php ./bin/console d:m:m --no-interaction
 php ./bin/console cache:clear
