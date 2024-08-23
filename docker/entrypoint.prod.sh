1ae492b7c6784ef4ccd49407ba7a14e83106688e
g.petraroli <g.petraroli@gmail.com>
2024-08-23 04:51:48 +0200

    [Docker] change ownership to the entire project folder

--- docker/entrypoint.prod.sh
+++ docker/entrypoint.prod.sh
@@ -9,15 +9,11 @@ php ./bin/console importmap:install
 php bin/console sass:build
 php bin/console asset-map:compile
 
-composer dump-env prod
-
 php ./bin/console d:m:m --no-interaction
 APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear
 
-mkdir -p "./data"
-mkdir -p "./var"
+mkdir -p "./data" "./var"
 
-chown -R www-data:www-data ./data
-chown -R www-data:www-data ./var
+chown -R www-data:www-data .
 
 php-fpm
